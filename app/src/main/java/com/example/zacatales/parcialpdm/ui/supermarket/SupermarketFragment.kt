459d7453974550d96bb42e077aebee832748fd68
CesarZacatales <00223021@uca.edu.sv>
2023-05-23 10:03:43 -0600

    Layer model

--- app/src/main/java/com/example/zacatales/parcialpdm/ui/supermarket/SupermarketFragment.kt
+++ app/src/main/java/com/example/zacatales/parcialpdm/ui/supermarket/SupermarketFragment.kt
@@ -1,10 +1,11 @@
-package com.example.zacatales.parcialpdm
+package com.example.zacatales.parcialpdm.ui.supermarket
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.zacatales.parcialpdm.R
 
 class SupermarketFragment : Fragment() {
 
